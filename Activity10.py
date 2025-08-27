d254b24ac71b72da9f4a824b53fde781b815d8e2
kenturk <100469540+kenturk@users.noreply.github.com>
2025-08-27 15:07:07 +0800

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,10 @@
+Username = "BrionesKen"
+Password = "hatdog123"
+
+uname = input("Enter Username --> ")
+pword = input("Enter Password --> ")
+
+if uname == Username and pword == Password:
+	print("Access Granted")
+else:
+	print("Access Denied")
